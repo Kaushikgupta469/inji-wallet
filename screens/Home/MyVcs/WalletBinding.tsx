0bb38f2db6d5a4bd3c4913ee781d2da09e25c546
KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>
2026-06-23 14:58:55 +0530

    fix(iOS): loader stuck on error of wallet binding (#2497)
    * fix(iOS): loader stuck on error of wallet binding
    
    Signed-off-by: KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>
    
    * refactor: add timeout ref in useOverlayVisibleAfterTimeout to avoid race conditions
    
    Signed-off-by: KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>
    
    * test: udpate missing test scenarios - useOverlayVisibleAfterTimeout
    
    Signed-off-by: KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>
    
    ---------
    
    Signed-off-by: KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>

--- screens/Home/MyVcs/WalletBinding.tsx
+++ screens/Home/MyVcs/WalletBinding.tsx
@@ -10,6 +10,8 @@ import {ActorRefFrom} from 'xstate';
 import {VCMetadata} from '../../../shared/VCMetadata';
 import {TelemetryConstants} from '../../../shared/telemetry/TelemetryConstants';
 import {VCItemMachine} from '../../../machines/VerifiableCredential/VCItemMachine/VCItemMachine';
+import {useOverlayVisibleAfterTimeout} from '../../../shared/hooks/useOverlayVisibleAfterTimeout';
+import {isAndroid} from '../../../shared/constants';
 
 export const WalletVerified: React.FC = () => {
   return (
@@ -25,6 +27,16 @@ export const WalletVerified: React.FC = () => {
 export const WalletBinding: React.FC<WalletBindingProps> = props => {
   const controller = useKebabPopUp(props);
   const {t} = useTranslation('WalletBinding');
+  const delay = isAndroid() ? 0 : 200;
+  const isWalletBindingInProgress = useOverlayVisibleAfterTimeout(
+    controller.walletBindingInProgress,
+    delay,
+  );
+  const isWalletBindingError = useOverlayVisibleAfterTimeout(
+    !!controller.walletBindingError,
+    delay,
+  );
+
   return (
     <>
       <BindingVcWarningOverlay
@@ -48,13 +60,16 @@ export const WalletBinding: React.FC<WalletBindingProps> = props => {
       )}
 
       <MessageOverlay
+        key="walletBindingError"
         testID="walletBindingError"
-        isVisible={controller.isWalletBindingError}
+        isVisible={isWalletBindingError}
         title={controller.walletBindingError}
+        onBackdropPress={controller.CANCEL}
         onButtonPress={controller.CANCEL}
       />
       <MessageOverlay
-        isVisible={controller.walletBindingInProgress}
+        key={'walletBindingProgress'}
+        isVisible={isWalletBindingInProgress}
         title={t('inProgress')}
         progress
       />
